b78c972d136937e2ed60d5894683e3f8806b4e66
dong <dong@gmail.com>
2020-12-23 20:45:59 -0500

    scrape different page for scraping dollar

--- bot/live_ticker_scrape.py
+++ bot/live_ticker_scrape.py
@@ -5,6 +5,7 @@ import bs4 as bs
 import json
 import time
 def scrape_live_ticker():
+    change_columns = ['name', 'last', 'change%'] 
     #maybe rotate user agent through a list ... 
     user_agent_list = [
 
@@ -13,28 +14,37 @@ def scrape_live_ticker():
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6"
     }
-    futures_url = 'https://www.investing.com/indices/indices-futures'
-    change_columns = ['name', 'last', 'change%'] 
 
-    res = requests.get('https://investing.com', headers=headers).content
-    futures_res = requests.get(futures_url, headers= headers).content
+    main_url        = 'https://investing.com' 
+    futures_url     = 'https://www.investing.com/indices/indices-futures'
+    currency_url    = 'https://www.investing.com/currencies/fx-futures'
 
-    df= pd.read_html(res)
-    futures_df = pd.read_html(futures_res)
+    main_res        =   requests.get(main_url, headers=headers).content
+    futures_res     =   requests.get(futures_url, headers= headers).content
+    currency_res    =   requests.get(currency_url, headers = headers).content
+    
+
+    df= pd.read_html(main_res)
+    futures_df      =   pd.read_html(futures_res)
+    currency_df     =   pd.read_html(currency_res)
+
     futures_df = futures_df[0]
     futures_df.drop(columns=["Unnamed: 0", 'Month', 'High', 'Low', 'Chg.','Time', 'Unnamed: 9'], axis=1, inplace=True)
     futures_df.columns = change_columns
     futures_df_dict =  futures_df.to_dict('records')
+
+
+    currency_df = currency_df[0]
+    currency_df.drop(columns=["Unnamed: 0", 'Month', 'High', 'Low', 'Chg.','Time', 'Unnamed: 9'], axis=1, inplace=True)
+    currency_df.columns = change_columns
+    currency_df_dict =  currency_df.to_dict('records')
+
     ### need to uncomment and run this to check index of the list of dataframes
     # count=0
     # for item in df:
     #     count +=1
     #     print("***************************")
     #     print(item)
-    index = df[7]
-    index.drop(columns=[0,3,5,6], axis=1, inplace=True)
-    index.columns = change_columns 
-    index_dict = index.to_dict("records")
 
     shitcoin= df[1]
     # print(shitcoin.columns)
@@ -46,11 +56,6 @@ def scrape_live_ticker():
     commodity.drop(columns=['Name', 'Month'], axis=1, inplace=True)
     commodity.columns = change_columns
     commodity_dict = commodity.to_dict("records")
-
-    dollar = df[8]
-    dollar.drop(columns=[0, 3, 5, 6], axis=1, inplace=True)
-    dollar.columns = change_columns
-    dollar_dict= dollar.to_dict("records")
 
     # data = {
     #     "index":index_dict ,
@@ -65,7 +70,7 @@ def scrape_live_ticker():
         'es':       futures_df_dict[1] ,
         'nas':      futures_df_dict[2],
         'vix':      futures_df_dict[4],
-        'dollar':   index_dict[-1],
+        'dollar':   currency_df_dict[0],
         'gold':     commodity_dict[0],
         'btc':      shitcoin_dict[0],
         'eth':      shitcoin_dict[1]
