e925563c14acc704828268029af2477f2961eb8c
dong <dong@gmail.com>
2021-01-09 01:19:15 -0500

    add gold bot back

--- bot/live_ticker_scrape.py
+++ bot/live_ticker_scrape.py
@@ -18,11 +18,13 @@ def scrape_live_ticker():
     main_url        = 'https://investing.com' 
     futures_url     = 'https://www.investing.com/indices/indices-futures'
     currency_url    = 'https://www.investing.com/currencies/fx-futures'
+    commodity_url   = 'https://www.investing.com/commodities/real-time-futures'
     shitcoin_url    = 'https://www.investing.com/crypto/currencies'
 
     main_res        =   requests.get(main_url, headers=headers).content
     futures_res     =   requests.get(futures_url, headers= headers).content
     currency_res    =   requests.get(currency_url, headers = headers).content
+    commodity_res   =   requests.get(commodity_url, headers = headers).content
     shitcoin_res    =   requests.get(shitcoin_url, headers = headers).content
 
     df= pd.read_html(main_res)
@@ -30,6 +32,7 @@ def scrape_live_ticker():
     futures_df      =   pd.read_html(futures_res)
     currency_df     =   pd.read_html(currency_res)
     shitcoin_df     =   pd.read_html(shitcoin_res)
+    commodity_df    =   pd.read_html(commodity_res)
 
     futures_df = futures_df[0]
     futures_df.drop(columns=["Unnamed: 0", 'Month', 'High', 'Low', 'Chg.','Time', 'Unnamed: 9'], axis=1, inplace=True)
@@ -51,31 +54,20 @@ def scrape_live_ticker():
     btc = [shitcoin for shitcoin in shitcoin_df_dict if shitcoin['name']== "BTC"][0]
     eth = [shitcoin for shitcoin in shitcoin_df_dict if shitcoin['name']== "ETH"][0]
 
+
+    commodity_df = commodity_df[0]
+    commodity_df.drop(columns=["Unnamed: 0", 'Month', 'High', 'Low', 'Chg.','Time', 'Unnamed: 9'], axis=1, inplace=True)
+    commodity_df.columns = change_columns
+    commodity_df_dict =  commodity_df.to_dict('records')
+
+    gold = [commodity for commodity in commodity_df_dict if commodity['name'] == 'Gold'][0]
+
     ### need to uncomment and run this to check index of the list of dataframes
     # count=0
     # for item in df:
     #     count +=1
     #     print("***************************")
     #     print(item)
-
-    # shitcoin= shitcoin_df[0]
-    # print(shitcoin.columns)
-    # print(shitcoin) 
-    # shitcoin.drop(columns=['Unnamed: 0', "Name", "Market Cap", "Vol (24H)"], axis=1, inplace=True)
-    # shitcoin.columns = change_columns 
-    # shitcoin_dict = shitcoin.to_dict("records")
-
-    commodity= df[4] 
-    commodity.drop(columns=['Name', 'Month'], axis=1, inplace=True)
-    commodity.columns = change_columns
-    commodity_dict = commodity.to_dict("records")
-
-    # data = {
-    #     "index":index_dict ,
-    #     "shitcoin": shitcoin_dict,
-    #     "commodity": commodity_dict, 
-    #     "dollar" : dollar_dict
-    # }
 
     ## cleaned data
     data = {
@@ -84,7 +76,7 @@ def scrape_live_ticker():
         'nas':      futures_df_dict[2],
         'vix':      futures_df_dict[4],
         'dollar':   currency_df_dict[0],
-        'gold':     commodity_dict[0],
+        'gold':     gold, 
         'btc':      btc, 
         'eth':      eth 
     }
