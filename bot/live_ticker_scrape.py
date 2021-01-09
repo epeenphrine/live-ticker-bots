c1aa8052ff7017d2270d63b11fbd3d8fdf6532ab
dong <dong@gmail.com>
2021-01-08 22:27:06 -0500

    fix scraper use new url for crypto

--- bot/live_ticker_scrape.py
+++ bot/live_ticker_scrape.py
@@ -18,15 +18,18 @@ def scrape_live_ticker():
     main_url        = 'https://investing.com' 
     futures_url     = 'https://www.investing.com/indices/indices-futures'
     currency_url    = 'https://www.investing.com/currencies/fx-futures'
+    shitcoin_url    = 'https://www.investing.com/crypto/currencies'
 
     main_res        =   requests.get(main_url, headers=headers).content
     futures_res     =   requests.get(futures_url, headers= headers).content
     currency_res    =   requests.get(currency_url, headers = headers).content
-    
+    shitcoin_res    =   requests.get(shitcoin_url, headers = headers).content
 
     df= pd.read_html(main_res)
+
     futures_df      =   pd.read_html(futures_res)
     currency_df     =   pd.read_html(currency_res)
+    shitcoin_df     =   pd.read_html(shitcoin_res)
 
     futures_df = futures_df[0]
     futures_df.drop(columns=["Unnamed: 0", 'Month', 'High', 'Low', 'Chg.','Time', 'Unnamed: 9'], axis=1, inplace=True)
@@ -39,6 +42,15 @@ def scrape_live_ticker():
     currency_df.columns = change_columns
     currency_df_dict =  currency_df.to_dict('records')
 
+    shitcoin_df = shitcoin_df[0]
+    shitcoin_df.drop(columns=["#", "Unnamed: 1", 'Name', 'Market Cap', 'Vol (24H)', 'Total Vol', 'Chg (7D)'],axis=1, inplace=True)
+    shitcoin_df.columns = change_columns
+    shitcoin_df_dict =  shitcoin_df.to_dict('records')
+
+    ## too much rows ... filter this way. Might be better 
+    btc = [shitcoin for shitcoin in shitcoin_df_dict if shitcoin['name']== "BTC"][0]
+    eth = [shitcoin for shitcoin in shitcoin_df_dict if shitcoin['name']== "ETH"][0]
+
     ### need to uncomment and run this to check index of the list of dataframes
     # count=0
     # for item in df:
@@ -46,11 +58,12 @@ def scrape_live_ticker():
     #     print("***************************")
     #     print(item)
 
-    shitcoin= df[1]
+    # shitcoin= shitcoin_df[0]
     # print(shitcoin.columns)
-    shitcoin.drop(columns=['Unnamed: 0', "Name", "Market Cap", "Vol (24H)"], axis=1, inplace=True)
-    shitcoin.columns = change_columns 
-    shitcoin_dict = shitcoin.to_dict("records")
+    # print(shitcoin) 
+    # shitcoin.drop(columns=['Unnamed: 0', "Name", "Market Cap", "Vol (24H)"], axis=1, inplace=True)
+    # shitcoin.columns = change_columns 
+    # shitcoin_dict = shitcoin.to_dict("records")
 
     commodity= df[4] 
     commodity.drop(columns=['Name', 'Month'], axis=1, inplace=True)
@@ -72,8 +85,8 @@ def scrape_live_ticker():
         'vix':      futures_df_dict[4],
         'dollar':   currency_df_dict[0],
         'gold':     commodity_dict[0],
-        'btc':      shitcoin_dict[0],
-        'eth':      shitcoin_dict[1]
+        'btc':      btc, 
+        'eth':      eth 
     }
     ## commented out incase want to save
     # with open('tickerScrape.json','w') as f:
